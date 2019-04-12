814fdbaecbf5d8c5649ace2fb46c70d0532af1da
Ian Be <ian84be@gmail.com>
2019-04-12 10:06:50 -0500

    test add/remove on usersModel, post on index

--- index.js
+++ index.js
@@ -3,17 +3,32 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 
+const knex = require('knex');
+const knexConfig = require('./knexfile.js');
+const db = knex(knexConfig.testing);
+
+const users = require('./usersModel.js');
+
 server.get('/', async (req,res) => {
     res.status(200).json({message:'yaya'});
 });
 
-server.post('/', async (req,res) => {
+server.post('/users', async (req,res) => {
     const {name,pass} = req.body;
-    res.status(201).json({name,pass});
+    if (!name || !pass) {
+        res.status(400).json({error: 'please provide name/pass'});
+    } else {
+        try {
+            const newUser = await users.insert(req.body);
+            if (newUser) {
+                res.status(201).json(newUser);
+            }
+        }
+        catch(err) {
+            res.status(500).json(err);
+        }
+    }
 });
 
 module.exports = server;
 
-const knex = require('knex');
-const knexConfig = require('./knexfile.js');
-const db = knex(knexConfig.testing);
--- index.spec.js
+++ index.spec.js
@@ -1,24 +1,35 @@
 
 const request = require('supertest');
 const server = require('./index.js');
+const db = require('./dbConfig.js');
 
 describe('index.js', () => {
-    it('should respond status 200', async () => {
-        const result = await request(server).get('/');
-        expect(result.status).toBe(200);
+    beforeEach(async () => {
+        await db('users').truncate();
     });
 
-    describe('CREATE', () => {
-        it('should respond status 201', async () => {
-            const resource = {name:'steve',pass:'dave'};
-            const result = await request(server).post('/', resource);
+    describe('get /', () => {
+        it('should respond status 200', async () => {
+            const result = await request(server).get('/');
+            expect(result.status).toBe(200);
+        });
+    });
+
+    describe('post /users', () => {
+        it('should respond status 201 if successful', async () => {
+            const user = {name:'steve',pass:'dave'};
+            const result = await request(server).post('/users').send(user);
             expect(result.status).toBe(201);
         });
 
-        it('should return the new resource, async () => {
-            // const resource = {name:'steve',pass:'dave'};
-            // const result = await request(server).post('/', resource);
-            expect(result.status).toBe(201);
+        it('should respond status 400 if name/pass is missing', async () => {
+            const user = {name:'dave'};
+            const result = await request(server).post('/users').send(user);
+            expect(result.status).toBe(400);
+
+            const user2 = {pass:'dave'};
+            const result2 = await request(server).post('/users').send(user2);
+            expect(result2.status).toBe(400);
         });
     });
 });
